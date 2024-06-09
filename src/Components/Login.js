c1449c90ad7f0a9da0ac8e3102fcfd2e5b069fb5
shivashankarareddy <ssr2tech@gmail.com>
2024-06-09 21:38:36 +0530

    SignIn page update

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -15,6 +15,7 @@ const Login = () => {
         <input type='text' placeholder='Email Address' className='p-2 my-2 w-full'/>
         <input type='password' placeholder='Password' className='p-2 my-2 w-full'/>
         <button className='p-2 my-4 bg-red-700 w-full'>Sign In</button>
+        <p>Are you new to MyTube? please SignUp</p>
 
       </form>
 
