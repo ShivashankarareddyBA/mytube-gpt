815177a71a60899578701bc83bf95b6fee022a81
shivashankarareddy <ssr2tech@gmail.com>
2024-06-10 15:08:20 +0530

    formValidation inprogress

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,21 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/formValidation";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
 
-const [isSignInForm, setIsSignInForm] = useState(true);
+  const email = useRef();
+  const password =useRef();
 
-const toggleSignInForm =()=>{
-  setIsSignInForm(!isSignInForm);
-};
+  const handleButtonClick = () => {
+     checkValidData(email , password);
+
+  };
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
 
   return (
     <div>
@@ -20,34 +28,44 @@ const toggleSignInForm =()=>{
         />
       </div>
       <form className=" w-3/12 absolute p-12 bg-black m-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-2xl mb-1">{isSignInForm? "Sign In" : "SignUp"}</h1>
-        {!isSignInForm &&<input
-          type="text"
-          placeholder="Full Name"
-          className="p-2 my-2 w-full"
-        />}
-        {!isSignInForm &&<input
-          type="num"
-          placeholder="Phone Number"
-          className="p-2 my-2 w-full"
-        />}
-        
+        <h1 className="font-bold text-2xl mb-1">
+          {isSignInForm ? "Sign In" : "SignUp"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-2 w-full"
+          />
+        )}
+        {!isSignInForm && (
+          <input
+            type="num"
+            placeholder="Phone Number"
+            className="p-2 my-2 w-full"
+          />
+        )}
+
         <input
+         ref={email}
           type="text"
           placeholder="Email Address"
           className="p-2 my-2 w-full"
         />
         <input
+        ref={password}
           type="password"
           placeholder="Password"
           className="p-2 my-2 w-full"
         />
-        <button className="p-2 my-4 bg-red-700 w-full">{isSignInForm ? "Sign In" : "Sign Up"}</button>
+        <button className="p-2 my-4 bg-red-700 w-full">
+          {isSignInForm ? "Sign In" : "Sign Up"} onClick={handleButtonClick}
+        </button>
         <p className="text-sm py-4">
-          Are you new to MyTube?
           <u onClick={toggleSignInForm} className="text-red-400 cursor-pointer">
-            
-            { isSignInForm ? "New to MyTube? SignUp Now" :"Allready Registered. Sign In Now"}
+            {isSignInForm
+              ? " Are New to MyTube? SignUp Now"
+              : "Allready Registered. Sign In Now"}
           </u>
         </p>
       </form>
