de2d3bf7c148bf3284cacdcc930ff28286c705e9
shivashankarareddy <ssr2tech@gmail.com>
2024-06-17 12:36:22 +0530

    Browse page 1st commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,30 +1,23 @@
-import React from 'react'
-import logo from '../img/logo-color.png'
-
-
+import React from "react";
+import logo from "../img/logo-color.png";
 
 const Header = () => {
   return (
-  <>
-    <div className='absolute ml-20 mt-4 bg-gradient-to-b from-black z-10 opacity-80'>
-      {/* log image */}
-    <img  className='w-44' src={logo} alt='logo'/>
-       
-    </div>
-     {/* <div className=' absolute '>
-        
-     <button onClick={Login} className=" font-bold text-white border border-black m-5 mx-[1100px] bg-red-500 px-2 w-20">
-  Sign In
-</button>
-
-     </div> */}
+    <>
+      <div className="absolute ml-20 mt-4 bg-gradient-to-b from-black z-10 opacity-80">
+        {/* log image */}
+        <img className="w-44" src={logo} alt="logo" />
+      </div>
+      <div className=" absolute ">
+        <button
+          onClick={"SignOut"}
+          className=" font-bold text-red-400 mt-5 mx-[1280px]  px-2 w-20"
+        >
+          SignOut
+        </button>
+      </div>
     </>
+  );
+};
 
-    
-    
-
-    
-  )
-}
-
-export default Header
+export default Header;
