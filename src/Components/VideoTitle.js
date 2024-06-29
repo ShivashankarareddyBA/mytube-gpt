8705288145701da3d3ec5f0e007acf9a122cc691
shivashankarareddy <ssr2tech@gmail.com>
2024-06-29 21:44:59 +0530

    video_title updated

--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -5,9 +5,9 @@ const VideoTitle = ({title, overview}) => {
     <div className='pt-40 px-15'>
       <h1 className='px-12 text-2xl font-bold my-2 text-gray-800'>{title}</h1>
       <p className='px-12 w-1/3 my-2 text-sm'>{overview}</p>
-      <div className=' px-14 '>
-        <button className='mx-2  rounded-lg border border-black bg-white text-black w-20 font-bold '>Play</button>
-        <button className='mx-2  rounded-lg border border-black bg-gray-200 text-black w-20 font-bold'>More</button>
+      <div className=' px-14   '>
+        <button className='mx-2  rounded-lg border border-gray-600 bg-white text-black w-28 font-bold bg-opacity-50'> ▶ Play</button>
+        <button className='mx-2 rounded-lg border border-gray-600 bg-gray-500 text-white w-28 font-bold bg-opacity-50'>More Info</button>
       </div>
 
     </div>
