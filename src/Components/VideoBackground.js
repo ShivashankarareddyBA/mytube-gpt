e4f93f729651b26208935d3c7df0bd5423be7a0c
shivashankarareddy <ssr2tech@gmail.com>
2024-07-01 15:32:56 +0530

    custom hook updated

--- src/Components/VideoBackground.js
+++ src/Components/VideoBackground.js
@@ -1,30 +1,9 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "../utils/movieSlice";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo); // also wek use this in reackhook use state
-  const dispatch = useDispatch();
-
-  const getMoviewVidos = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/1022789/videos?language=en-US",
-      API_OPTIONS
-    );
-
-    const json = await data.json();
-    console.log(json);
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-    dispatch(addTrailerVideo(trailer));
-  };
-
-  useEffect(() => {
-    getMoviewVidos();
-  }, []);
+  useMovieTrailer();
 
   return (
     <div>
--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,6 +1,29 @@
+import { useDispatch } from "react-redux";
+import {API_OPTIONS} from "../utils/constants"
+import {addTrailerVideo} from "../utils/movieSlice"
+import { useEffect } from "react";
 
 const useMovieTrailer = () => {
-    
+    const dispatch = useDispatch();
+
+    const getMoviewVidos = async () => {
+      const data = await fetch(
+        "https://api.themoviedb.org/3/movie/1022789/videos?language=en-US",
+        API_OPTIONS
+      );
+  
+      const json = await data.json();
+      console.log(json);
+  
+      const filterData = json.results.filter((video) => video.type === "Trailer");
+      const trailer = filterData.length ? filterData[0] : json.results[0];
+      console.log(trailer);
+      dispatch(addTrailerVideo(trailer));
+    };
+  
+    useEffect(() => {
+      getMoviewVidos();
+    }, []);
   
 }
 
