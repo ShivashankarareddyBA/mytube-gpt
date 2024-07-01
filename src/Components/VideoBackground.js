8732b4b8a362c6103a8c96716958ee7d0220b0a6
shivashankarareddy <ssr2tech@gmail.com>
2024-07-01 17:07:07 +0530

    background video CSS has been updated..

--- src/Components/VideoBackground.js
+++ src/Components/VideoBackground.js
@@ -6,9 +6,9 @@ const VideoBackground = ({ movieId }) => {
   useMovieTrailer(movieId);
 
   return (
-    <div>
-      <iframe
-        src={"https://www.youtube.com/embed/" + trailerVideo?.key}
+    <div  className="w-screen">
+      <iframe className="w-screen aspect-video h-[600px]"
+        src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
