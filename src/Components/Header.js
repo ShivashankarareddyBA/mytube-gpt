38671bf297bbe0bb3c22c1f8f8eb654c55858e80
shivashankarareddy <ssr2tech@gmail.com>
2024-07-01 18:59:12 +0530

    logo changed.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import logo from "../img/logo-color.png";
+import logo from "../img/logo.JPG";
 import { useNavigate } from "react-router-dom";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
@@ -42,7 +42,7 @@ const Header = () => {
 
   return (
     <div className=" flex justify-between absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10">
-      <img className="w-44 h-30" src={logo} alt="logo" />
+      <img className="w-40 h-10" src={logo} alt="logo" />
       {user && (
         <div className="flex p-2">
           <img
